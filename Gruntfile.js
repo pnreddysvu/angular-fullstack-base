6179f11625b1b0e7bc5d810391d43ae135593c76
Rumman Ahmed <ahmed.rumman@gmail.com>
2015-01-23 01:55:25 +1100

    polymer custom element directory added to grunt-contrib-watch list

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,7 +90,9 @@ module.exports = function (grunt) {
           '{.tmp,<%= yeoman.client %>}/{app,components}/**/*.js',
           '!{.tmp,<%= yeoman.client %>}{app,components}/**/*.spec.js',
           '!{.tmp,<%= yeoman.client %>}/{app,components}/**/*.mock.js',
-          '<%= yeoman.client %>/assets/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}'
+          '<%= yeoman.client %>/assets/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= yeoman.client %>/elements/{,*//*}*.{png,jpg,jpeg,gif,webp,svg,html,js,json}',
+
         ],
         options: {
           livereload: true
