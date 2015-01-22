18d7e59ff08990044cfc14d69515b46ffb7e429c
Rumman Ahmed <ahmed.rumman@gmail.com>
2015-01-23 02:32:52 +1100

    smoothscroll menu added for angular app framework

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,13 +1,19 @@
 'use strict';
 
 angular.module('serveMeApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
+  .controller('MainCtrl', function ($scope, $http, socket, $location, $anchorScroll) {
     $scope.awesomeThings = [];
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
       socket.syncUpdates('thing', $scope.awesomeThings);
     });
+
+    // scroll to feature included for SPA App
+    $scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+    };
 
     $scope.addThing = function() {
       if($scope.newThing === '') {
