9d959500e90a037524db819be15fe1bdea982a8b
Rumman Ahmed <ahmed.rumman@gmail.com>
2015-05-07 01:07:36 +1000

    and deferred function understood

--- client/app/custdir/custdir.directive.js
+++ client/app/custdir/custdir.directive.js
@@ -1,11 +1,19 @@
 'use strict';
 
 angular.module('serveMeApp')
-  .directive('custdir', function (dataSrv) {
+  .directive('custdir', function (dataSrv,$q,$timeout,$log) {
     return {
       templateUrl: 'app/custdir/custdir.html',
       restrict: 'EA',
       link: function (scope, element, attrs) {
+
+        scope.okGreet  = function(name){
+          if (name == "rumman"){
+            return true;
+          }else{
+            return false;
+          }
+         }
 
         scope.reddit     = new dataSrv();
 
@@ -13,6 +21,33 @@ angular.module('serveMeApp')
 
       	console.log("element is : ", element);
       	console.log("each attrs is : ", attrs);
+
+        scope.deferedtest =  function (name) {
+          var deferred = $q.defer();
+
+          $timeout(function(){
+            deferred.notify("About to greet  "+ name);
+            if(scope.okGreet(name)){
+              deferred.resolve("hello " +  name);
+            }else{
+              deferred.reject("Greeting " + name + " is not allowed");
+            }
+          },1000)
+
+          return deferred.promise;
+         };
+
+        var promise = scope.deferedtest("rumman");
+
+        promise.then(function(greeting){
+          $log.info("Success " + greeting);
+        },
+        function(reason){
+          $log.error("Failed " + reason);
+        }),
+        function(update){
+          $log.warn("Got notification " + update);
+        } 
 
       	scope.changeDiv = function(){
       		element.hide();
